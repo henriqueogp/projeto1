869852f2fee8f08c51c970da70835b7be41ce246
Henrique de Oliveira <henriqueogp@hotmail.com>
2025-03-20 20:39:28 -0300

    Created a recipes directory inside the templates directory (which is inside the recipes directory). This was done so that I could specify 'recipes/home.html' as the path to the home.html template.

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,7 @@ def home(request):
     # defined in recipes.apps, must be added to the INSTALLED_APPS list in
     # projeto.settings in order for Django to look for the template file
     # ('home.html') inside the recipes directory.
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 
 def contact(request):
