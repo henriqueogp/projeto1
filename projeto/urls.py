20779a68d8a37c49c87a9fbccc2a734c450eb8d7
Henrique de Oliveira <henriqueogp@hotmail.com>
2025-03-20 19:51:52 -0300

    Removed "my_view" from projeto.urls and created two new views (pages) in recipes.views: home and contact

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
 
-
-def my_view(request):
-    return HttpResponse('Just a string')
-
+from recipes.views import contact, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', my_view)
+    path('', home),
+    path('contact/', contact)
 ]
